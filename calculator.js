a934358c65b260f7e500b81139732642bc8306ca
Pallavi Supahiya <pallavi.supahiya@catalyst.sh>
2021-02-04 19:40:43 +0530

    Second Commit

--- calculator.js
+++ calculator.js
@@ -1,12 +1,23 @@
 let output = document.getElementById('output');
 buttons = document.querySelectorAll('button');
 let outputValue = '';
+function validate(evt) {
+    var vl = output.value;
+    vl = vl.split("").filter((v) => ((v >= 0 && v <= 9) || (v == '+') || (v == '-') || (v == '*') || (v == "/") || (v == "%") || (v == "(") || (v == ")") || (v == "^"))).join("");
+    output.value = vl;
+    outputValue = vl;   
+}
 for (item of buttons) {
     item.addEventListener('click', (e)=>{
         buttonText = e.target.innerText;
         console.log("Button Text is", buttonText)
-        if (buttonText == 'X') {
+        if (buttonText == 'x') {
             buttonText = '*';
+            outputValue += buttonText;
+            output.value = outputValue;
+        }
+        else if (buttonText == '÷') {
+            buttonText = '/';
             outputValue += buttonText;
             output.value = outputValue;
         }
@@ -17,6 +28,11 @@ for (item of buttons) {
         else if (buttonText == 'C')
         {
             outputValue = '';
+            output.value = outputValue;
+        }
+        else if (buttonText == 'del')
+        {
+            outputValue = outputValue.substr(0, outputValue.length - 1);
             output.value = outputValue;
         }
         else {
